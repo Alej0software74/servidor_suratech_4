0b86f82e18576a91515e094bf4455114b4bd24b5
you@example.com <you@example.com>
2024-09-17 15:08:28 -0500

    susura

---
+++ src/main/java/com/example/servidor_suratech_4/MODELOS/Paciente.java
@@ -0,0 +1,18 @@
+package com.example.servidor_suratech_4.MODELOS;
+
+import java.time.LocalDate;
+
+public class Paciente {
+
+    private Integer id;
+    private String nombre;
+    private Integer edad;
+    private String documento;
+    private LocalDate fechanacimiento;
+    private String telefono;
+    private Integer copago;
+    private String email;
+    private String direccion;
+    private String medicofamiliar;
+
+}
